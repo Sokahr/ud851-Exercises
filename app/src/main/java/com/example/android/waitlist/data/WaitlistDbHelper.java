0b05c7d346f1b3cfc5d6fe1c37cd710b80f47c11
Benjamin Peter <ben61279@gmail.com>
2017-11-26 22:02:25 +0100

    Exercise done

--- app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,22 +1,82 @@
 package com.example.android.waitlist.data;
 
-// TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.waitlist.data.WaitlistContract.WaitlistEntry;
 
-    // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+// DONE (1) extend the SQLiteOpenHelper class
+public class WaitlistDbHelper extends SQLiteOpenHelper {
+    // DONE (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+    static final String DATABASE_NAME = "witlist.db";
+    // DONE (3) Create a static final int called DATABASE_VERSION and set it to 1
+    static final int DATABASE_VERSION = 1;
+    // DONE(4) Create a Constructor that takes a context and calls the parent constructor
 
-    // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
+    /**
+     * Create a helper object to create, open, and/or manage a database.
+     * This method always returns very quickly.  The database is not actually
+     * created or opened until one of {@link #getWritableDatabase} or
+     * {@link #getReadableDatabase} is called.
+     *
+     * @param context to use to open or create the database
+     */
+    public WaitlistDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-    // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+    // DONE (5) Override the onCreate method
 
-    // TODO (5) Override the onCreate method
-
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
         // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
+        final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " + WaitlistEntry.TABLE_NAME + " (" +
+                WaitlistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                WaitlistEntry.COLUMN_GUEST_NAME + " TEXT NOT NULL," +
+                WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL," +
+                WaitlistEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                ");";
 
         // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
+        db.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
 
-    // TODO (8) Override the onUpgrade method
+
+    // Done (8) Override the onUpgrade method
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     * <p>
+     * <p>
+     * The SQLite ALTER TABLE documentation can be found
+     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     * </p><p>
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     * </p>
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + WaitlistEntry.TABLE_NAME);
+        onCreate(db);
 
         // TODO (9) Inside, execute a drop table query, and then call onCreate to re-create it
+    }
+
 
 }
