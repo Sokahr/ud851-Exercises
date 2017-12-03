d0872dbfd43b1803a6992584d750e29a4bbf530e
Benjamin Peter <ben61279@gmail.com>
2017-12-03 14:11:06 +0100

    Exercise done

--- app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -23,7 +23,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
 /**
@@ -40,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
 
+    private TextView mWord;
+    private TextView mDefinition;
+
+
     // This state is when the word definition is hidden and clicking the button will therefore
     // show the definition
     private final int STATE_HIDDEN = 0;
@@ -47,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
     // This state is when the word definition is shown and clicking the button will therefore
     // advance the app to the next word
     private final int STATE_SHOWN = 1;
+    private int wordColIndex;
+    private int definitionColIndex;
 
 
     @Override
@@ -55,8 +61,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the views
-        // TODO (1) You'll probably want more than just the Button
+        // DONE (1) You'll probably want more than just the Button
         mButton = (Button) findViewById(R.id.button_next);
+        mWord = (TextView) findViewById(R.id.text_view_word);
+        mDefinition = (TextView) findViewById(R.id.text_view_definition);
 
         //Run the database operation to get the cursor off of the main thread
         new WordFetchTask().execute();
@@ -86,12 +94,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Change button text
         mButton.setText(getString(R.string.show_definition));
-
-        // TODO (3) Go to the next word in the Cursor, show the next word and hide the definition
+        mDefinition.setText("");
+        // Done (3) Go to the next word in the Cursor, show the next word and hide the definition
         // Note that you shouldn't try to do this if the cursor hasn't been set yet.
         // If you reach the end of the list of words, you should start at the beginning again.
         mCurrentState = STATE_HIDDEN;
-
+        if(mData!=null){
+            if(!mData.moveToNext()) {
+                mData.moveToFirst();
+            }
+            mWord.setText(mData.getString(wordColIndex));
+        }
     }
 
     public void showDefinition() {
@@ -99,15 +112,18 @@ public class MainActivity extends AppCompatActivity {
         // Change button text
         mButton.setText(getString(R.string.next_word));
 
-        // TODO (4) Show the definition
+        // DONE (4) Show the definition
         mCurrentState = STATE_SHOWN;
-
+        if(mData!=null){
+            mDefinition.setText(mData.getString(definitionColIndex));
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // TODO (5) Remember to close your cursor!
+        // DONE (5) Remember to close your cursor!
+        mData.close();
     }
 
     // Use an async task to do the data fetch off of the main thread.
@@ -136,8 +152,12 @@ public class MainActivity extends AppCompatActivity {
             // Set the data for MainActivity
             mData = cursor;
 
-            // TODO (2) Initialize anything that you need the cursor for, such as setting up
+            // Done (2) Initialize anything that you need the cursor for, such as setting up
             // the screen with the first word and setting any other instance variables
+            wordColIndex = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
+            definitionColIndex = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
+            cursor.moveToFirst();
+            mWord.setText(cursor.getString(wordColIndex));
         }
     }
 
